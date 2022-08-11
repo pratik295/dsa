d1f0d671db5f8c1458498a00b2fc6fef284d6409
Pratik Harsh <pratikharsh295@gmail.com>
2022-08-11 18:57:07 +0530

    unitj

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Node{
+int data;
+Node *next;
+Node(int data){
+    this->data=data;
+    next=NULL:
+}
+Node* InputNode(){
+
+    int data;
+    cin>>data;
+    Node *head= NULL;
+    Node *tail=NULL;
+    while(data!=-1){
+        Node *n
+    }
+}
+
+};
